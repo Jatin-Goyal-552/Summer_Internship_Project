bbbbb5e1a7cbcd97233fd7421d9ec5915548fd24
JatinGoyal <56165691+Jatin-Goyal-552@users.noreply.github.com>
2022-06-19 12:20:12 +0530

    deployed on cloud

--- AssesmentWebsite/AssesmentWebsite/urls.py
+++ AssesmentWebsite/AssesmentWebsite/urls.py
@@ -16,3 +16,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api2.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
